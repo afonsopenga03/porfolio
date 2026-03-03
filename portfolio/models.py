47db7b24d8e0eb0575c9ae77a67804ad037d8728
afonsopenga03 <gregbrizzy@gmail.com>
2026-03-03 14:02:58 -0800

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,8 @@
+# models.py
+
 from django.db import models
+from cloudinary_storage.storage import MediaCloudinaryStorage
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -8,15 +12,27 @@ class Category(models.Model):
         return self.name
 
 
-
 class Project(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='projects')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='projects'
+    )
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class ProjectImage(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='projects/')
-    
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name='images'
+    )
+    # FORÇAR o storage do Cloudinary diretamente no campo
+    image = models.ImageField(
+        upload_to='projects/',
+        storage=MediaCloudinaryStorage(),
+    )
+
+    def __str__(self):
+        return f"Imagem de {self.project.title}"
